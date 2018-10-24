f8340f1993e45dd559a9ede2994c4f8bcaa2e8e3
HaticeTueysuez <34030703+HaticeTueysuez@users.noreply.github.com>
2018-10-24 16:14:54 +0200

    Aufgabe 2

---
+++ Aufgabe 2/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Text;
+
+namespace Aufgabe_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+/*        var i = 42;
+        var pi = 3.1415;
+        var salute = "Hello, World";
+
+        var variable = 0;
+        Console.WriteLine(variable);
+//__________________________________________________________//
+        
+        int[] ia = {1, 0, 2, 9, 3, 8, 4, 7, 5, 6};
+        int ergebnis = ia[2] * ia[8] + ia[4];
+
+        Console.WriteLine(ergebnis);
+        Console.WriteLine(ia.Length);
+//__________________________________________________________//
+
+        double[] da = new double [3];
+        da[0] = 3.1415;
+        da[1] = 2.718281828459;
+        da[2] = 2;
+
+        Console.WriteLine(da.Length);
+
+//__________________________________________________________//
+
+        string meinString = "Dies ist ein String";
+        char zeichen = meinString[5];
+
+        string a = "eins";
+        string b = "zwei";
+        string c = "eins";
+        bool a_eq_b = (a == b);
+        bool a_eq_c = (a == c);
+
+        Console.WriteLine(c);
+        Console.WriteLine(a_eq_c);
+
+//_________________________________________________________//
+
+
+        int zahl1 = int.Parse(Console.ReadLine());
+        int zahl2 = int.Parse(Console.ReadLine());
+
+        if (zahl1 > 3 && zahl2==6)
+        {
+            Console.WriteLine("Du hast gewonnen");
+        }
+
+        else
+        {
+            Console.WriteLine("Leider verloren");
+        }
+//________________________________________________________//    
+
+        //int m = int.Parse(Console.ReadLine());
+        string m = Console.ReadLine();
+        switch (m)
+        {
+            case "eins":
+                Console.WriteLine("Du hast EINS eingegeben");
+                break;
+            case "zwei":
+                Console.WriteLine("ZWEI war Deine Wahl");
+                break;
+            case "drei":
+                Console.WriteLine("Du tipptest eine DREI");
+                break;
+            case "elf": 
+                Console.WriteLine("11 war deine Wahl");
+                break;
+            default:
+                Console.WriteLine("Die Zahl " + m + " kenne ich nicht");
+                break;
+}
+
+//_________________________________________________________//  
+
+        string m = Console.ReadLine();
+
+        if (m == "eins")
+        {
+            Console.WriteLine("Du hast EINS eingegeben");
+        }
+        if (m == "zwei")
+        {
+            Console.WriteLine("ZWEI war Deine Wahl");
+        }
+        if (m == "drei")
+        {
+            Console.WriteLine("Du tipptest eine DREI");
+        }
+        if (m == "elf")
+        {
+            Console.WriteLine("11 war deine Wahl");
+        }
+
+        else
+        {
+            Console.WriteLine("Die Zahl " + m + " kenne ich nicht");
+        }
+
+//______________________________________________________//   
+
+        int[] zahlen={1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        int x=0;
+        while (x <10)
+        {
+            Console.WriteLine(zahlen[x].ToString());
+            x++;
+        }
+//______________________________________________________// */
+        
+        string[] someStrings = 
+        {
+            "Hier",
+            "sehen",
+            "wir",
+            "einen",
+            "Array",
+            "von",
+            "Strings"
+        };
+        
+        
+        /* for (int i = 0; i < someStrings.Length; i++)
+        {
+            Console.WriteLine(someStrings[i]);
+        }*/
+        
+        
+        /* int i = 0;
+        while (i < someStrings.Length)
+        {
+            Console.WriteLine(someStrings[i].ToString());
+            i++;
+        }*/
+
+        /* int i = 0;
+        do 
+        {
+            Console.WriteLine(someStrings[i]);
+            i++;
+        }
+        while (i < someStrings.Length);*/
+
+        /* int i = 0;
+        while (true)
+        {
+            Console.WriteLine(someStrings[i]);
+            if (i >= someStrings.Length)
+                break;
+            i++;
+        }*/
+
+        foreach (string s in someStrings)
+        {
+            Console.WriteLine(s);
+        }
+
+        }
+    }
+}
